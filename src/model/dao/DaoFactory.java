c5dfd4196a0576a7561f6b2aa637d9b20314215e
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-05 21:34:15 -0300

    :sparkles: feat: adding static method createDepartmentDao to the DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,14 @@
 package model.dao;
 
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC();
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC();
 	}
 }
