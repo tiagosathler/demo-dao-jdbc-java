3cd14f777c7d157d4169d1c16832c1ac3c1016fd
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-05 21:33:05 -0300

    :sparkles: feat: creating DepartmentDaoJDBC with basic implementation

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,32 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+	@Override
+	public void insert(Department obj) {
+	}
+
+	@Override
+	public void update(Department obj) {
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		return null;
+	}
+
+}
